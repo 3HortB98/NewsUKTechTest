53682a76d27ca2034a026c5f7327188a6f424413
Ben Horton <benhorton16@gmail.com>
2024-05-02 17:42:47 +0100

    added/fix some unit test

--- app/src/test/java/com/example/newsuktechtest/NetworkTest.kt
+++ app/src/test/java/com/example/newsuktechtest/NetworkTest.kt
@@ -3,7 +3,11 @@ package com.example.newsuktechtest
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.example.newsuktechtest.data.NetworkClient
+import com.example.newsuktechtest.data.models.Coin
+import com.example.newsuktechtest.data.models.CoinDetails
 import com.example.newsuktechtest.data.models.Coins
+import com.example.newsuktechtest.data.models.Whitepaper
+import com.example.newsuktechtest.ui.coins.CoinDetailsUiState
 import com.example.newsuktechtest.ui.coins.CoinsUiState
 import com.example.newsuktechtest.ui.coins.CoinsViewModel
 import io.mockk.coEvery
@@ -27,6 +31,27 @@ class NetworkTest {
     private lateinit var vm: CoinsViewModel
     private lateinit var mockCoins: Coins
     private val expectedResponseData = Coins(emptyList())
+    private lateinit var mockCoinDetails: Coin
+    private val coinID = "coin"
+
+    private val expectedResponseDataDetails = Coin(
+        name = "Coin",
+        type = "token",
+        id = coinID,
+        coinDetails = CoinDetails(
+            symbol = "C",
+            logo = "https://static.coinpaprika.com/coin/btc-bitcoin/logo.png",
+            isActive = true,
+            openSource = false,
+            description = "some test of some sort. some more words to make it a description",
+            developmentStatus = "working product",
+            startedAt = "2009-01-03T00:00:00Z",
+            whitepaper = Whitepaper(
+                link = "link",
+                thumbnail = "thumbnail"
+            )
+        )
+    )
     private val dispatcher = StandardTestDispatcher()
 
     @get:Rule
@@ -39,8 +64,11 @@ class NetworkTest {
     fun setup(){
         Dispatchers.setMain(dispatcher)
         mockCoins = expectedResponseData
+        mockCoinDetails = expectedResponseDataDetails
         coEvery { mockGetRepository.getCoins() } returns Result.success(mockCoins)
+        coEvery { mockGetRepository.getCoinDetail(coinID) } returns Result.success(mockCoinDetails)
         vm = CoinsViewModel(mockGetRepository,TestDispatcherProvider())
+
     }
 
     @After
@@ -48,32 +76,56 @@ class NetworkTest {
         Dispatchers.resetMain()
     }
 
-    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
     fun `test for getting data and return is successful`() = runTest{
 
-        var stateValue = vm.uiState.value
-        assertEquals(CoinsUiState.Loading,stateValue)
-        backgroundScope.launch{
+
+        launch{
             vm.getData()
         }
         dispatcher.scheduler.advanceUntilIdle()
-        stateValue = vm.uiState.value
+        val stateValue = vm.uiState.value
         assertEquals(CoinsUiState.Success(expectedResponseData),stateValue)
 
     }
 
-    @OptIn(ExperimentalCoroutinesApi::class)
+
     @Test
     fun `test for getting data and return is failure`() = runTest {
-        var stateValue = vm.uiState.value
-        assertEquals(CoinsUiState.Loading,stateValue)
+
         coEvery { mockGetRepository.getCoins() } returns Result.failure(Exception("Network Error"))
-        backgroundScope.launch{
+        launch{
             vm.getData()
         }
         dispatcher.scheduler.advanceUntilIdle()
-        stateValue = vm.uiState.value
+        val stateValue = vm.uiState.value
         assertEquals(CoinsUiState.Error(Result.failure<Exception>(Exception("Network Error")).toString()),stateValue)
+    }
+
+    @Test
+       fun `test for getting details of coin and return is successful`() = runTest{
+
+           val startStateValue = vm.uiDetailsState.value
+           assertEquals(CoinDetailsUiState.Hidden,startStateValue)
+           launch{
+               vm.getCoinDetail(coinID)
+           }
+           dispatcher.scheduler.advanceUntilIdle()
+           val stateValue = vm.uiDetailsState.value
+           assertEquals(CoinDetailsUiState.Success(expectedResponseDataDetails),stateValue)
+
+       }
+
+    @Test
+    fun `test for getting details of coin and return is failure`() = runTest {
+        val startStateValue = vm.uiDetailsState.value
+        assertEquals(CoinDetailsUiState.Hidden,startStateValue)
+        coEvery { mockGetRepository.getCoinDetail(coinID) } returns Result.failure(Exception("Network Error"))
+        launch{
+            vm.getCoinDetail(coinID)
+        }
+        dispatcher.scheduler.advanceUntilIdle()
+        val stateValue = vm.uiDetailsState.value
+        assertEquals(CoinDetailsUiState.Error(Result.failure<Exception>(Exception("Network Error")).toString()),stateValue)
     }
 }
